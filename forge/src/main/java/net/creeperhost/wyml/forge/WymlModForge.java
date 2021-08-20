3a8ae2f8d36140f622a5c51b97e83a1c67c86897
Travis_N <travis_n@creeperhost.net>
2021-08-20 15:35:04 +0100

    Make it so the mod is not needed clientside

--- forge/src/main/java/net/creeperhost/wyml/forge/WymlModForge.java
+++ forge/src/main/java/net/creeperhost/wyml/forge/WymlModForge.java
@@ -1,7 +1,11 @@
 package net.creeperhost.wyml.forge;
 
 import net.creeperhost.wyml.WhyYouMakeLag;
+import org.apache.commons.lang3.tuple.Pair;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(WhyYouMakeLag.MOD_ID)
 public class WymlModForge
@@ -9,5 +13,6 @@ public class WymlModForge
     public WymlModForge()
     {
         WhyYouMakeLag.init();
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, ()-> Pair.of(()-> FMLNetworkConstants.IGNORESERVERONLY, (net, save)->true));
     }
 }
